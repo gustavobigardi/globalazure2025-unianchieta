9d4a9cb2e867094989556fcd7f239646f2fbf295
Gustavo Bellini Bigardi <gustavobellinibigardi@Gustavos-MacBook-Pro.local>
2025-05-08 11:36:56 -0300

    init

---
+++ BlazorAppAnchieta/BlazorAppAnchieta/Program.cs
@@ -0,0 +1,38 @@
+using BlazorAppAnchieta.Client.Pages;
+using BlazorAppAnchieta.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorAppAnchieta.Client._Imports).Assembly);
+
+app.Run();
